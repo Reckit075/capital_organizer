bb5cb7a4a8ddf10f85b01fb536e0c7b77b98f1f4
Reckit075 <mateusz.majka.zsl@gmail.com>
2021-02-25 18:08:21 +0100

    add form validation

--- components/form.vue
+++ components/form.vue
@@ -1,32 +1,52 @@
 <template>
-  <v-form v-model="valid">
+  <v-form ref="form" v-model="valid" lazy-validation>
     <v-container>
       <v-row>
-        <v-col cols="12" md="4">
-          <v-text-field
-            v-model="firstname"
-            :rules="nameRules"
-            :counter="10"
-            label="First name"
-            required
-          ></v-text-field>
-        </v-col>
-
-        <v-col cols="12" md="4">
-          <v-text-field
-            v-model="email"
-            :rules="emailRules"
-            label="E-mail"
-            required
-          ></v-text-field>
-        </v-col>
+        <v-text-field
+          v-model="name"
+          :counter="10"
+          :rules="nameRules"
+          placeholder="Name"
+          required
+        ></v-text-field>
+        <br />
+        <v-text-field
+          v-model="email"
+          :rules="emailRules"
+          placeholder="E-mail"
+          required
+        ></v-text-field>
       </v-row>
-      <v-btn elevation="4" large>submit</v-btn>
+      <v-btn
+        elevation="4"
+        :disabled="!valid"
+        color="success"
+        large
+        @click="validate"
+        >submit</v-btn
+      >
     </v-container>
   </v-form>
 </template>
-<script>
+<script type="text/javascript">
 export default {
-  name: "Form",
+  data: () => ({
+    valid: true,
+    name: "",
+    nameRules: [
+      (v) => !!v || "Name is required",
+      (v) => (v && v.length <= 10) || "Name must be less than 10 characters",
+    ],
+    email: "",
+    emailRules: [
+      (v) => !!v || "E-mail is required",
+      (v) => /.+@.+\..+/.test(v) || "E-mail must be valid",
+    ],
+  }),
+  methods: {
+    validate() {
+      this.$refs.form.validate();
+    },
+  },
 };
 </script>

d0496b24cd52f447886ebbd1e2a50059acb8116d
Reckit075 <mateusz.majka.zsl@gmail.com>
2021-03-21 20:10:46 +0100

    add basic panel(without connect with API)

---
+++ components/panel.vue
@@ -0,0 +1,48 @@
+<template>
+  <v-form
+    @submit="sendData"
+    ref="form"
+    method="post"
+    v-model="valid"
+    lazy-validation
+  >
+    <v-text-field label="Amount" value="10.00" prefix="$"></v-text-field>
+    <v-select :items="items" label="Choose type" solo></v-select>
+    <v-btn
+      elevation="4"
+      :disabled="!valid"
+      color="success"
+      large
+      type="submit"
+      >submit</v-btn
+    >
+    <v-btn
+      elevation="4"
+      :disabled="!valid"
+      color="success"
+      large
+      type="submit"
+      >reset</v-btn
+    >
+  </v-form>
+</template>
+<script>
+import Vue from "vue";
+import axios from "axios";
+import VueAxios from "vue-axios";
+
+Vue.use(VueAxios, axios);
+
+export default {
+  data: () => ({
+    valid: true,
+    items: ["income", "expenditure"],
+  }),
+  methods: {
+    sendData() {
+      //send Data to server
+      //get response from server
+    },
+  },
+};
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,16 +1,16 @@
 <template>
   <div>
     <h1>Capital Organizer Main Page</h1>
-    <Form form-type="login"></Form>
+    <Panel />
   </div>
 </template>
 
 <script>
-import Form from "../components/form";
+import Panel from "../components/panel";
 export default {
   name: "App",
   components: {
-    Form,
+    Panel,
   },
 };
 </script>

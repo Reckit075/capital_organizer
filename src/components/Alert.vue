47a1db1072981f7adba7c12e381383aaff0da0bd
Reckit075 <mateusz.majka.zsl@gmail.com>
2021-05-14 21:55:27 +0200

    add alert component and use in form.

---
+++ src/components/Alert.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <v-alert v-if="success=='true'" type="success">{{ text }}</v-alert>
+    <v-alert v-else-if="success=='false'" type="error">{{ text }}</v-alert>
+  </div>
+</template>
+<script>
+export default {
+  name: "Alert",
+  props: {
+      success:{
+          type: String,
+          required:true
+      },
+      text:{
+          type:String,
+          required: true
+      }
+  }
+};
+</script>
